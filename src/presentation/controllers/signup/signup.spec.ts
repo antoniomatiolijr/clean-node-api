d10a1926de41c06d66db56af8fa71cbb69b8f9b0
antoniomatiolijr <antoniomatiolijr@gmail.com>
2020-07-05 10:23:09 -0300

    refactor: move HttpHelper to a folder

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -7,7 +7,7 @@ import {
 } from './signup-protocols'
 import { MissingParamError, ServerError } from '../../errors'
 import { HttpRequest } from '../../protocols'
-import { ok, serverError, badRequest } from '../../helpers/http-helper'
+import { ok, serverError, badRequest } from '../../helpers/http/http-helper'
 
 const makeFakeRequest = (): HttpRequest => ({
   body: {
