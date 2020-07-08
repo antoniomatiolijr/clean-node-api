071b9b9435314a264cb3a9392b002a041faedeb0
antoniomatiolijr <antoniomatiolijr@gmail.com>
2020-07-07 21:01:25 -0300

    test: ensure DbAuthenticatiom return null if LoadRepositoryAccountByEmail returns null

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -58,4 +58,13 @@ describe('DbAuthentication use case', () => {
     const promise = sut.auth(makeAuthentication())
     await expect(promise).rejects.toThrow()
   })
+
+  test('Should return null if LoadAccountByEmailRepository returns null', async () => {
+    const { sut, loadAccountByEmailRepositoryStub } = makeSut()
+    jest
+      .spyOn(loadAccountByEmailRepositoryStub, 'load')
+      .mockReturnValueOnce(null)
+    const accessToken = await sut.auth(makeAuthentication())
+    expect(accessToken).toBeNull()
+  })
 })
