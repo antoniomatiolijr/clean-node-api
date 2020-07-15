451b3fe3be3b28b9b767c70997d0d4d084cfa088
antoniomatiolijr <antoniomatiolijr@gmail.com>
2020-07-15 20:49:37 -0300

    feat: add login route

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -1,6 +1,10 @@
 import request from 'supertest'
 import app from '../config/app'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+import { Collection } from 'mongodb'
+import { hash } from 'bcrypt'
+
+let accoutCollection: Collection
 
 describe('Login Routes', () => {
   beforeAll(async () => {
@@ -12,7 +16,7 @@ describe('Login Routes', () => {
   })
 
   beforeEach(async () => {
-    const accoutCollection = await MongoHelper.getCollection('accounts')
+    accoutCollection = await MongoHelper.getCollection('accounts')
     await accoutCollection.deleteMany({})
   })
 
@@ -25,6 +29,25 @@ describe('Login Routes', () => {
           email: 'matioli@gmail.com',
           password: 'matioli',
           passwordConfirmation: 'matioli'
+        })
+        .expect(200)
+    })
+  })
+
+  describe('POST /login', () => {
+    test('Should return 200 on login', async () => {
+      const password = await hash('matioli', 12)
+      await accoutCollection.insertOne({
+        name: 'Matioli',
+        email: 'matioli@gmail.com',
+        password
+      })
+
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'matioli@gmail.com',
+          password: 'matioli'
         })
         .expect(200)
     })
