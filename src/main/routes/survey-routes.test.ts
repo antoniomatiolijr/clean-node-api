6d3b701c707313548690f296bb947fc705300509
antoniomatiolijr <antoniomatiolijr@gmail.com>
2020-08-01 15:56:11 -0300

    feat: add AddSurvey composition

---
+++ src/main/routes/survey-routes.test.ts
@@ -0,0 +1,37 @@
+import request from 'supertest'
+
+import { Collection } from 'mongodb'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+import app from '../config/app'
+
+let surveyCollection: Collection
+
+describe('Survey Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    surveyCollection = await MongoHelper.getCollection('surveys')
+    await surveyCollection.deleteMany({})
+  })
+
+  describe('POST /surveys', () => {
+    test('Should return 204 on add survey success', async () => {
+      await request(app)
+        .post('/api/surveys')
+        .send({
+          question: 'any_question',
+          answers: [
+            { image: 'any_image', answer: 'any_answer' },
+            { answer: 'any_answer2' }
+          ]
+        })
+        .expect(204)
+    })
+  })
+})
