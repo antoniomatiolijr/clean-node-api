6551e9d05b1d3e459b87eda53b1f3d613c84c23a
antoniomatiolijr <antoniomatiolijr@gmail.com>
2020-08-06 23:39:28 -0300

    refactor: organize test in describe blocks

--- src/infra/criptography/bcrypt-adapter/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter/bcrypt-adapter.spec.ts
@@ -16,57 +16,61 @@ const makeSut = (): BcryptAdapter => {
 }
 
 describe('Bcrypt Adapter', () => {
-  test('Should call hash with correct value', async () => {
-    const hashSpy = jest.spyOn(bcrypt, 'hash')
-    const sut = makeSut()
-    await sut.hash('any_value')
-    expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
-  })
-  test('Should returns a valid hash on hash success', async () => {
-    const sut = makeSut()
-    const hashValue = await sut.hash('any_value')
-    expect(hashValue).toBe('hash_value')
-  })
+  describe('hash()', () => {
+    test('Should call hash with correct value', async () => {
+      const hashSpy = jest.spyOn(bcrypt, 'hash')
+      const sut = makeSut()
+      await sut.hash('any_value')
+      expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
+    })
+    test('Should returns a valid hash on hash success', async () => {
+      const sut = makeSut()
+      const hashValue = await sut.hash('any_value')
+      expect(hashValue).toBe('hash_value')
+    })
 
-  test('Should throw if hash throws', async () => {
-    const sut = makeSut()
-    jest
-      .spyOn(bcrypt, 'hash')
-      .mockReturnValue(new Promise((resolve, reject) => reject(new Error())))
+    test('Should throw if hash throws', async () => {
+      const sut = makeSut()
+      jest
+        .spyOn(bcrypt, 'hash')
+        .mockReturnValue(new Promise((resolve, reject) => reject(new Error())))
 
-    const promise = sut.hash('any_value')
-    await expect(promise).rejects.toThrow()
-  })
-
-  test('Should call compare with correct value', async () => {
-    const hashSpy = jest.spyOn(bcrypt, 'compare')
-    const sut = makeSut()
-    await sut.compare('any_value', 'any_hash')
-    expect(hashSpy).toHaveBeenCalledWith('any_value', 'any_hash')
+      const promise = sut.hash('any_value')
+      await expect(promise).rejects.toThrow()
+    })
   })
 
-  test('Should returns true when compare succeeds', async () => {
-    const sut = makeSut()
-    const isValid = await sut.compare('any_value', 'any_hash')
-    expect(isValid).toBe(true)
-  })
+  describe('compare()', () => {
+    test('Should call compare with correct value', async () => {
+      const hashSpy = jest.spyOn(bcrypt, 'compare')
+      const sut = makeSut()
+      await sut.compare('any_value', 'any_hash')
+      expect(hashSpy).toHaveBeenCalledWith('any_value', 'any_hash')
+    })
 
-  test('Should returns false when compare fails', async () => {
-    const sut = makeSut()
-    jest
-      .spyOn(bcrypt, 'compare')
-      .mockReturnValueOnce(new Promise((resolve) => resolve(false)))
-    const isValid = await sut.compare('any_value', 'any_hash')
-    expect(isValid).toBe(false)
-  })
+    test('Should returns true when compare succeeds', async () => {
+      const sut = makeSut()
+      const isValid = await sut.compare('any_value', 'any_hash')
+      expect(isValid).toBe(true)
+    })
 
-  test('Should throw if compare throws', async () => {
-    const sut = makeSut()
-    jest
-      .spyOn(bcrypt, 'compare')
-      .mockReturnValue(new Promise((resolve, reject) => reject(new Error())))
+    test('Should returns false when compare fails', async () => {
+      const sut = makeSut()
+      jest
+        .spyOn(bcrypt, 'compare')
+        .mockReturnValueOnce(new Promise((resolve) => resolve(false)))
+      const isValid = await sut.compare('any_value', 'any_hash')
+      expect(isValid).toBe(false)
+    })
 
-    const promise = sut.compare('any_value', 'any_hash')
-    await expect(promise).rejects.toThrow()
+    test('Should throw if compare throws', async () => {
+      const sut = makeSut()
+      jest
+        .spyOn(bcrypt, 'compare')
+        .mockReturnValue(new Promise((resolve, reject) => reject(new Error())))
+
+      const promise = sut.compare('any_value', 'any_hash')
+      await expect(promise).rejects.toThrow()
+    })
   })
 })
