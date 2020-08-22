4384a27adcbcd081c642402020fda1d2eb8f5b23
antoniomatiolijr <antoniomatiolijr@gmail.com>
2020-08-22 10:54:30 -0300

    docs: add survey path

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,6 +1,21 @@
 import { loginPath } from './paths/login-path'
-import { accountSchema, loginParamsSchema, errorSchema } from './schemas'
-import { badRequest, notFound, serverError, unauthorized } from './components'
+import {
+  accountSchema,
+  loginParamsSchema,
+  errorSchema,
+  surveysSchema,
+  surveyAnswerSchema,
+  surveySchema,
+  apiKeyAuthSchema
+} from './schemas'
+import {
+  badRequest,
+  forbidden,
+  notFound,
+  serverError,
+  unauthorized
+} from './components'
+import { surveyPath } from './paths'
 
 export default {
   openapi: '3.0.0',
@@ -21,20 +36,31 @@ export default {
   tags: [
     {
       name: 'Login'
+    },
+    {
+      name: 'Enquete'
     }
   ],
   paths: {
-    '/login': loginPath
+    '/login': loginPath,
+    '/surveys': surveyPath
   },
   schemas: {
     account: accountSchema,
     loginParams: loginParamsSchema,
-    error: errorSchema
+    error: errorSchema,
+    surveys: surveysSchema,
+    survey: surveySchema,
+    surveyAnswer: surveyAnswerSchema
   },
   components: {
+    securitySchemes: {
+      apiKeyAuth: apiKeyAuthSchema
+    },
     badRequest,
     serverError,
     unauthorized,
-    notFound
+    notFound,
+    forbidden
   }
 }
